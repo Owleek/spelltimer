5f0682d9f140eceed9b6da650a632596a9441228
owleek <owleek@MacBook-Pro-owleek.local>
2024-12-19 17:49:57 +0300

    initital structure, next step state management

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import ReactDom from 'react-dom/client';
+import App from './App';
 import './index.scss';
 
 const rootElement = document.getElementById('root');
 
 if (!rootElement) {
     throw new Error('\'root\' not found');
-}
-
-const App = () => {
-    return <div>Root screen</div>
 }
 
 const root = ReactDom.createRoot(rootElement);
