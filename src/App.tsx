10be0ef7ed2f7e9b80b1ea4de33059ee1a4dc294
owleek <owleek@MacBook-Pro-owleek.local>
2025-01-13 21:03:36 +0300

    sketching concept - activity screen, main workspace, overlay + sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@
 // 3. Утилиты и бизнес-логика.
 // 4. Стили и ассеты.
 
+import { useState } from "react";
 import Welcome from "./screens/welcome/Welcome";
+import Activity from "./screens/activity/Activity";
+
 
 const App = () => <div className="appContainer">
-    <Welcome />
+    {/* <Welcome /> */}
+    <Activity />
 </div>;
 
 export default App;
