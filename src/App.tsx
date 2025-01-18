8ac9a82b88f2f5a59fbc6dd5eb6635752a643988
owleek <owleek@MacBook-Pro-owleek.local>
2025-01-18 19:01:58 +0300

    broken routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@
 // 2. Компоненты вашего проекта.
 // 3. Утилиты и бизнес-логика.
 // 4. Стили и ассеты.
-
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./screens/welcome/Welcome";
 import Activity from "./screens/activity/Activity";
 
 
-const App = () => <div className="appContainer">
-    {/* <Welcome /> */}
-    <Activity />
-</div>;
+const App = () => {
+    return (
+        <Router>
+            <div className="appContainer">
+                <Routes>
+                    <Route path="/" element={<Welcome />}></Route>
+                    <Route path="/activities" element={<Activity />}></Route>
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
--- src/screens/activity/Constructor.tsx
+++ src/screens/activity/Constructor.tsx
@@ -3,6 +3,7 @@
 // 3. Утилиты и бизнес-логика.
 // 4. Стили и ассеты.
 import React from 'react';
+import { Routes, Link, Route } from 'react-router-dom';
 import './activity.scss';
 
 interface IProps {
@@ -13,24 +14,29 @@ const Constructor = (props: IProps) => <React.Fragment>
     <div className="layout has-sidebar fixed-sidebar fixed-header">
       <aside id="sidebar" className="sidebar break-point-sm has-bg-image">
         <div className="sidebar-layout">
-
           <div className="sidebar-content">
             <nav className="menu open-current-submenu">
               <ul>
                 <li className="Menu__item">
-                  <div className="Menu__tile img-abilities">
-                    <span>SPELS</span>
-                  </div>
+                  {/* <Link to="/activities/spels"> */}
+                    <div className="Menu__tile img-abilities">
+                      <span>SPELS</span>
+                    </div>
+                  {/* </Link> */}
                 </li>
                 <li className="Menu__item">
-                  <div className="Menu__tile img-items">
-                    <span>ITEMS</span>
-                  </div>
+                  {/* <Link to="/activities/items"> */}
+                    <div className="Menu__tile img-items">
+                      <span>ITEMS</span>
+                    </div>
+                  {/* </Link> */}
                 </li>
                 <li className="Menu__item">
-                  <div className="Menu__tile img-other">
-                    <span>OTHER</span>
-                  </div>
+                  {/* <Link to="/activities/other"> */}
+                    <div className="Menu__tile img-other">
+                      <span>OTHER</span>
+                    </div>
+                  {/* </Link> */}
                 </li>
               </ul>
             </nav>
@@ -43,26 +49,30 @@ const Constructor = (props: IProps) => <React.Fragment>
       <div className="overlay__workspace">
         <input type='text' className="searh" />
         <div className="tileGrid">
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
-          <i className="tileGrid__item"></i>
+          <Routes>
+            <Route path="spels" element={<React.Fragment>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            <i className="tileGrid__item"></i>
+            </React.Fragment>} />
+          </Routes>
         </div>
       </div>
     </div>
