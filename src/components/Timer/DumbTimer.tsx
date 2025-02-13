bc483321b27ab5f2c87a2f19b776e21f186586e3
owleek <owleek@MacBook-Pro-owleek.local>
2025-02-13 23:09:02 +0300

    made countdown animation, it works good

--- src/components/Timer/DumbTimer.tsx
+++ src/components/Timer/DumbTimer.tsx
@@ -27,6 +27,13 @@ const DumbTimer = ({ability, isTimeRuns}: IProps): JSX.Element => {
     const countMSRef = useRef<number>(0);
     const cooldownRef = useRef<number>(ability.cooldown[0]);
     const timerStatusRef = useRef<ETimeStatus>(ETimeStatus.ready);
+    const strokeDashoffsetRef = useRef<number>(0);
+
+    const radius = 30;
+    const length = 2 * Math.PI * radius;
+    const step = length / (COUNT_OF_BLINKS_EQUIVALENT_TO_ONE_SECOND * ability.cooldown[0]);
+
+    const circleRef = useRef<SVGCircleElement | null>(null);
 
     useEffect(() => {
         const instance = TickNotifier.getInstance();
@@ -37,32 +44,58 @@ const DumbTimer = ({ability, isTimeRuns}: IProps): JSX.Element => {
     useEffect(() => {
         if (!isTimeRuns && timerStatusRef.current === ETimeStatus.running) {
             timerStatusRef.current = ETimeStatus.paused;
-            return;
-        } 
+        }
 
-        timerStatusRef.current = timerStatusRef.current === ETimeStatus.paused ? ETimeStatus.running : timerStatusRef.current;
+        if (isTimeRuns && timerStatusRef.current === ETimeStatus.paused) {
+            timerStatusRef.current = ETimeStatus.running;
+        }
     }, [isTimeRuns]);
 
     const onTickNotify = () => {
-        if (cooldownRef.current === 0 || timerStatusRef.current !== ETimeStatus.running) return;
-        if (countMSRef.current < COUNT_OF_BLINKS_EQUIVALENT_TO_ONE_SECOND) {
-            countMSRef.current += 1;
+        const _countMS = countMSRef.current;
+        const _cooldown = cooldownRef.current;
+        const _timerStatus = timerStatusRef.current;
+        const _circle = circleRef.current;
+        const _strokeDashoffset = strokeDashoffsetRef.current;
+
+        if (_cooldown === 0 || _timerStatus !== ETimeStatus.running || !_circle) return;
+
+        if (_countMS === COUNT_OF_BLINKS_EQUIVALENT_TO_ONE_SECOND) {
+            countMSRef.current = 0;
+            cooldownRef.current -= 1;
+            return setCooldown(cooldownRef.current);
+        }
+
+        countMSRef.current += 1;
+        strokeDashoffsetRef.current += _strokeDashoffset + step < length ? step : length - _strokeDashoffset;
+        _circle.setAttribute('stroke-dashoffset', `${strokeDashoffsetRef.current}`);
+    }
+    
+    const handleClickTimer = () => {
+        if (!isTimeRuns || timerStatusRef.current === ETimeStatus.running) {
+            // showSpellToolbar
             return;
         }
 
-        countMSRef.current = 0;
-        cooldownRef.current -= 1;
-        setCooldown(cooldownRef.current);
+        timerStatusRef.current = ETimeStatus.running
     }
 
-    const setTimerStatusMain = () => {
-        if (!isTimeRuns) return;
-        timerStatusRef.current = timerStatusRef.current !== ETimeStatus.running ? ETimeStatus.running : ETimeStatus.paused;
-    }
-    
-    const handleClickTimer = () => setTimerStatusMain();
-
-    return <div className="DumbTimer" onClick={handleClickTimer}>{cooldown}</div>
+    return <div className="DumbTimer" onClick={handleClickTimer}>
+            <svg className="timer_svg" width="120" height="120" viewBox="0 0 120 120">
+                    <circle ref={circleRef} className="Timer__circle"
+                        cx="60"
+                        cy="60"
+                        strokeWidth="60"
+                        r={radius}
+                        fill="transparent"
+                        strokeDasharray={length}
+                        strokeDashoffset="0"
+                        transform="rotate(-90 60 60)"
+                        style={{ transition: `stroke-dashoffset 0.05s linear` }}
+                    />
+            </svg>
+            {cooldown}
+    </div>
 }
 
 export default DumbTimer;
