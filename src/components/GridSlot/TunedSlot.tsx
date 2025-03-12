d4083487796ca176730e9b11e12b417604a993de
owleek <owleek@MacBook-Pro-owleek.local>
2025-03-12 18:22:57 +0300

    artifact owner sticker, bugs still no fixed

--- src/components/GridSlot/TunedSlot.tsx
+++ src/components/GridSlot/TunedSlot.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './GridSlot.scss';
-import { ITimerData } from '../../data/data';
+import { ITimerData, EAbility } from '../../data/data';
 import fetchData from '../../data/data';
 
 interface IProps {
@@ -10,18 +10,18 @@ interface IProps {
 
 const TunedSlot = ({data, handleRemove}: IProps) => {
   const{ heroes } = fetchData;
-  const currentHero = data.owner ? heroes.find(hero => hero.name === data.owner) : null;
+  const currentHero = data.owner && data.type === EAbility.ARTIFACTS ? heroes.find(hero => hero.name === data.owner) : null;
   
-
   return (
     <div className="TunedSlot" style={{backgroundImage: `url("${data.img}")`}}>
-      { data.owner && <div className="TunedSlot__ownerBox"><img src={currentHero?.img}/></div> }
-      <div className="TunedSlot__ownerBox">
-      </div>
+      { 
+        currentHero && 
+          <div className="TunedSlot__ownerBox">
+              <img className="TunedSlot__ownerImg" src={currentHero.img}/>
+              <span className="TunedSlot__ownerName">{currentHero.name}</span>
+          </div> 
+      }
       <div className="TunedSlot__RemoveButton" onClick={() => handleRemove(data)}></div>
-      <div className="TunedSlot__boundKey">
-        <span>{data.boundKey}</span>
-      </div>
     </div>
   );
 };
