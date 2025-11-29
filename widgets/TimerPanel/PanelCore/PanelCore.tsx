eb3ece0bf05d0044cebae894d548c4e8941ccabe
owleek <owleek@MacBook-Pro-owleek.local>
2025-11-29 21:43:14 +0300

    refactor(TimerPanel): start of panel rebuild

---
+++ widgets/TimerPanel/PanelCore/PanelCore.tsx
@@ -0,0 +1,10 @@
+import './PanelCore.scss'
+
+export default function PanelCore() {
+
+    return (
+        <div className="PanelCore">
+            <div style={{width: '100px', height: '350px'}}></div>
+        </div>
+    );
+}
---
+++ widgets/TimerPanel/PanelToolbar/PanelToolbar.tsx
@@ -0,0 +1,9 @@
+import './PanelToolbar.scss'
+
+export default function PanelToolbar() {
+    return (
+        <div className="PanelToolbar">
+        </div>
+    )
+}
+
